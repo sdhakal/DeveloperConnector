1eeff44640f846bacdfe28f510c0341a86e5d345
Suman Dhakal <suman007dhakal@gmail.com>
2025-08-30 14:39:17 +0200

    Fixed dependencies issues

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,18 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
-
 const middleware = [thunk];
+
+// Prefer DevTools compose if available, else fallback to Redux compose
+const composeEnhancers =
+  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
