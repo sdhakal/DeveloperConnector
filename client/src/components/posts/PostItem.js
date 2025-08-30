449897b640da7b7f81a382df74d5f8e390fb9b1c
Suman Dhakal <suman007dhakal@gmail.com>
2025-08-30 23:15:17 +0200

    delete post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -6,50 +6,52 @@ import { Link } from "react-router-dom";
 import { deletePost, addLike, removeLike } from "../../actions/postActions";
 
 class PostItem extends Component {
-  onDeleteClick(id) {
+  onDeleteClick = (id) => {
     this.props.deletePost(id);
-  }
+  };
 
-  onLikeClick(id) {
+  onLikeClick = (id) => {
     this.props.addLike(id);
-  }
+  };
 
-  onUnlikeClick(id) {
+  onUnlikeClick = (id) => {
     this.props.removeLike(id);
-  }
+  };
 
-  findUserLike(likes) {
+  findUserLike = (likes = []) => {
     const { auth } = this.props;
-    if (likes.filter(like => like.user === auth.user.id).length > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+    const myId = auth?.user?.id || auth?.user?._id || "";
+    return likes.some((like) => String(like.user) === String(myId));
+  };
 
   render() {
     const { post, auth, showActions } = this.props;
+    const myId = auth?.user?.id || auth?.user?._id || "";
+    const isOwner = String(post.user) === String(myId);
 
     return (
       <div className="card card-body mb-3">
         <div className="row">
           <div className="col-md-2">
-            <a href="profile.html">
+            {/* If you have profile links/handles available, you can swap this <a> for a <Link> */}
+            <a href="#!">
               <img
                 className="rounded-circle d-none d-md-block"
                 src={post.avatar}
-                alt=""
+                alt={post.name || "user avatar"}
               />
             </a>
             <br />
             <p className="text-center">{post.name}</p>
           </div>
+
           <div className="col-md-10">
             <p className="lead">{post.text}</p>
+
             {showActions ? (
               <span>
                 <button
-                  onClick={this.onLikeClick.bind(this, post._id)}
+                  onClick={() => this.onLikeClick(post._id)}
                   type="button"
                   className="btn btn-light mr-1"
                 >
@@ -58,29 +60,33 @@ class PostItem extends Component {
                       "text-info": this.findUserLike(post.likes)
                     })}
                   />
-                  <span className="badge badge-light">{post.likes.length}</span>
+                  <span className="badge badge-light">
+                    {(post.likes && post.likes.length) || 0}
+                  </span>
                 </button>
+
                 <button
-                  onClick={this.onUnlikeClick.bind(this, post._id)}
+                  onClick={() => this.onUnlikeClick(post._id)}
                   type="button"
                   className="btn btn-light mr-1"
                 >
                   <i className="text-secondary fas fa-thumbs-down" />
                 </button>
+
                 <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
                   Comments
                 </Link>
-                {post.user === auth.user.id && (
+
+                {isOwner && (
                   <button
+                    onClick={() => this.onDeleteClick(post._id)}
                     type="button"
                     className="btn btn-danger mr-1"
-                    onClick={() => deletePost(post._id)}
                     title="Delete post"
                   >
                     <i className="fas fa-times" />
                   </button>
                 )}
-                ) : null}
               </span>
             ) : null}
           </div>
@@ -99,14 +105,12 @@ PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   deletePost: PropTypes.func.isRequired,
   addLike: PropTypes.func.isRequired,
-  removeLike: PropTypes.func.isRequired
+  removeLike: PropTypes.func.isRequired,
+  showActions: PropTypes.bool
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { deletePost, addLike, removeLike }
-)(PostItem);
+export default connect(mapStateToProps, { deletePost, addLike, removeLike })(PostItem);
